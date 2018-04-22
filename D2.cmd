65a60b72fedb60f638d9b1378aeeeff78620a659
Lonny Strunk <lonny.strunk@gmail.com>
2018-04-22 16:48:09 -0700

    Adding initial Condor files"

---
+++ D2.cmd
@@ -0,0 +1,9 @@
+universe        = vanilla
+executable      = src/D2.sh
+getenv          = true
+output          = output/D2.out
+error           = output/D2.err
+log             = output/D2.log
+arguments       = "/dropbox/17-18/573/Data/Documents/training/2009/UpdateSumm09_test_topics.xml"
+transfer_executable = false
+queue
---
+++ src/D2.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+cd ..
+
+if [ ! -d "py36" ]
+then
+    virtualenv py36 -p /opt/python-3.6/bin/python3
+    pip install -r src/ requirements.txt
+fi
+
+if [ ! -L "AQUAINT" ] || [ ! -L "AQUIANT-2" ]
+then
+    ln -sf /corpora/LDC/LDC02T31/ AQUAINT
+    ln -sf /corpora/LDC/LDC08T25/ AQUAINT-2
+fi
+
+. py36/bin/activate
+python src/d2system.py $@
+deactivate
