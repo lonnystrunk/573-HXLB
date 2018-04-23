3430849d11f92e4059c7b0bb17e157dd57a489e6
Xiaopei <36488154+xiaopeiwu@users.noreply.github.com>
2018-04-22 17:35:17 -0700

    Update D2.cmd

--- D2.cmd
+++ D2.cmd
@@ -4,6 +4,6 @@ getenv          = true
 output          = outputs/D2.out
 error           = outputs/D2.err
 log             = outputs/D2.log
-arguments       = "/dropbox/17-18/573/Data/Documents/training/2009/UpdateSumm09_test_topics.xml"
+arguments       = "/dropbox/17-18/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml"
 transfer_executable = false
 queue
