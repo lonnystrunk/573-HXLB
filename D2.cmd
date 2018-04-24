8e966d5748941293f6425cdb537316429fc60e71
Xiaopei <36488154+xiaopeiwu@users.noreply.github.com>
2018-04-23 22:04:00 -0700

    changing argument to the devtest dir

--- D2.cmd
+++ D2.cmd
@@ -4,6 +4,6 @@ getenv          = true
 output          = outputs/D2.out
 error           = outputs/D2.err
 log             = outputs/D2.log
-arguments       = "/dropbox/17-18/573/Data/Documents/devtest/GuidedSumm10_test_topics.xml"
+arguments       = "/dropbox/17-18/573/Data/Documents/devtest/"
 transfer_executable = false
 queue
